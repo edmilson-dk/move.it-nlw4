4e33f452111c0ba136d313abf77f77f21b8d594c
edmilson-dk <droidvidaboa@gmail.com>
2021-02-24 11:33:35 -0300

    feat: added profile component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,19 @@
 import Head from 'next/head';
 
+import styles from '../styles/pages/Home.module.css';
+
 import { ExperienceBar } from '../components/ExperienceBar';
+import { Profile } from '../components/Profile';
 
 function Home() {
   return (
-    <div className="container">
+    <div className={styles.container}>
       <ExperienceBar />
 
       <section>
-        <article></article>
+        <article>
+          <Profile />
+        </article>
         <article></article>
       </section>
     </div>
