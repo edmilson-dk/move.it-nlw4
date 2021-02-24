d96ab77747e99eeef8291d020328385b3cc2a19f
edmilson-dk <droidvidaboa@gmail.com>
2021-02-24 11:32:09 -0300

    feat: created profile component

---
+++ src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile() {
+  return (
+    <div>
+      <img src="/profile.jpg" alt="User profile"/>
+      <div>
+        <strong>Edmilson Jesus</strong>
+        <span>Level 1</span>
+      </div>
+    </div>
+  )
+}
