88aa8e662bdc8f5cd3fccfc336d12067addeedb2
edmilson-dk <droidvidaboa@gmail.com>
2021-02-24 10:04:31 -0300

    feat: created primary version of expericen bar component

---
+++ src/components/ExperienceBar.js
@@ -0,0 +1,11 @@
+function ExperienceBar() {
+  return (
+    <header className="experience-bar">
+      <span>0 xp</span>
+      <div></div>
+      <span>600 xp</span>
+    </header>
+  )
+}
+
+export default ExperienceBar;
