29def1c542af8cfbbb4116be15d6efc02ac5ce35
edmilson-dk <droidvidaboa@gmail.com>
2021-02-25 10:25:53 -0300

    fead: added level state

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,9 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
   return (
     <div className={styles.profileContainer}>
       <img src="/profile.jpg" alt="User profile"/>
@@ -8,7 +11,7 @@ export function Profile() {
         <strong>Edmilson Jesus</strong>
         <p>
           <img src="icons/level.svg" alt="User level"/>
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
