4ccf26232092d62204f907a65a60bebbd2c7336c
edmilson-dk <droidvidaboa@gmail.com>
2021-02-25 10:26:43 -0300

    feat: added completeChallenge function

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -3,7 +3,8 @@ import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox() {
-  const { activeChallenge, resetActiveChallenge } = useContext(ChallengesContext);
+
+  const { activeChallenge, resetActiveChallenge, completeChallenge } = useContext(ChallengesContext);
 
   return (
     <div className={styles.challengeBoxContainer}>
@@ -28,6 +29,7 @@ export function ChallengeBox() {
             <button
               type="button"
               className={styles.challengeBoxSuccededButton}
+              onClick={completeChallenge}
             >
               Completei
             </button>
